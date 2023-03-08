0724c7ef6afd333da9007e66fded6644a991701c
Thomas Obuya <thomasobuya95@gmail.com>
2023-03-08 15:26:39 +0300

    Add decode_word function

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -41,6 +41,10 @@ def decode_char(morse_char)
   MORSE_CODE[morse_char]
 end
 
+def decode_word(morse_word)
+  morse_word.split.map { |morse_char| decode_char(morse_char) }.join
+end
+
 def decode(message)
   message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
 end
